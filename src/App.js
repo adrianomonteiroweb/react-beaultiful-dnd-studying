95842a8b4cc285ae506bf79a9250ed85004c24de
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2022-12-07 13:29:22 -0300

    feat: array of objects list

--- src/App.js
+++ src/App.js
@@ -1,56 +1,32 @@
 import React from 'react';
+
 import './App.css';
 
-const finalSpaceCharacters = [
-  {
-    id: 'gary',
-    name: 'Gary Goodspeed',
-    thumb: '/images/gary.png'
-  },
-  {
-    id: 'cato',
-    name: 'Little Cato',
-    thumb: '/images/cato.png'
-  },
-  {
-    id: 'kvn',
-    name: 'KVN',
-    thumb: '/images/kvn.png'
-  },
-  {
-    id: 'mooncake',
-    name: 'Mooncake',
-    thumb: '/images/mooncake.png'
-  },
-  {
-    id: 'quinn',
-    name: 'Quinn Ergon',
-    thumb: '/images/quinn.png'
-  }
-]
+import finalSpaceCharacters from './data/finalSpaceCharacters';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Final Space Characters</h1>
-        <ul className="characters">
-          {finalSpaceCharacters.map(({id, name, thumb}) => {
+        <ul className='characters'>
+          {finalSpaceCharacters.map(({ id, name, thumb }) => {
             return (
               <li key={id}>
-                <div className="characters-thumb">
+                <div className='characters-thumb'>
                   <img src={thumb} alt={`${name} Thumb`} />
                 </div>
-                <p>
-                  { name }
-                </p>
+                <p>{name}</p>
               </li>
             );
           })}
         </ul>
       </header>
       <p>
-        Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
+        Images from{' '}
+        <a href='https://final-space.fandom.com/wiki/Final_Space_Wiki'>
+          Final Space Wiki
+        </a>
       </p>
     </div>
   );
--- src/data/finalSpaceCharacters.js
+++ src/data/finalSpaceCharacters.js
@@ -1,7 +1,27 @@
-export const finalSpaceCharacters = [
+export default [
   {
     id: 'gary',
     name: 'Gary Goodspeed',
     thumb: '/images/gary.png',
+  },
+  {
+    id: 'cato',
+    name: 'Little Cato',
+    thumb: '/images/cato.png',
+  },
+  {
+    id: 'kvn',
+    name: 'KVN',
+    thumb: '/images/kvn.png',
+  },
+  {
+    id: 'mooncake',
+    name: 'Mooncake',
+    thumb: '/images/mooncake.png',
+  },
+  {
+    id: 'quinn',
+    name: 'Quinn Ergon',
+    thumb: '/images/quinn.png',
   },
 ];
